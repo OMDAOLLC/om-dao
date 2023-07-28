0a098556dce8887d45ddccfeb62db0d088574d7d
Artem Gladkov <artem.gladkovw@gmail.com>
2023-07-28 12:12:33 +0300

    fix: Refactored WaitingProjects.tsx

--- src/features/show-projects/ui/ActiveProjects.tsx
+++ src/features/show-projects/ui/ActiveProjects.tsx
@@ -1,44 +1,41 @@
 import { FC, useMemo } from 'react';
-import { Project } from './Project';
+import { IProjectProps, Project } from './Project';
 
 import { useTranslation } from 'react-i18next';
 import {
   COMMON_AG_DATA,
   COMMON_LED_DATA,
   COMMON_TIGR_DATA,
-  ETokenSymbols,
 } from '../../../shared/constants/blockchain';
+import { TFunction } from 'i18next';
 
-const DEFAULT_ACTIVE_PROJECTS = [
-  {
-    title: COMMON_TIGR_DATA.title,
-    symbol: COMMON_TIGR_DATA.symbol,
-    href: COMMON_TIGR_DATA.herf,
-  },
-  {
-    title: COMMON_LED_DATA.title,
-    symbol: COMMON_LED_DATA.symbol,
-    href: COMMON_LED_DATA.herf,
-  },
-  {
-    title: COMMON_AG_DATA.title,
-    symbol: COMMON_AG_DATA.symbol,
-    href: COMMON_AG_DATA.herf,
-  },
-];
+const getActiveProjects = (t: TFunction) => {
+  const projects: IProjectProps[] = [
+    {
+      title: COMMON_TIGR_DATA.title,
+      symbol: COMMON_TIGR_DATA.symbol,
+      href: COMMON_TIGR_DATA.herf,
+    },
+    {
+      title: COMMON_LED_DATA.title,
+      symbol: COMMON_LED_DATA.symbol,
+      href: COMMON_LED_DATA.herf,
+    },
+    {
+      title: COMMON_AG_DATA.title,
+      symbol: COMMON_AG_DATA.symbol,
+      href: COMMON_AG_DATA.herf,
+      buttonName: t('common.swap'),
+    },
+  ];
+
+  return projects;
+};
 
 export const ActiveProjects: FC = () => {
   const { t } = useTranslation();
 
-  const activeProjects = useMemo(() => {
-    return DEFAULT_ACTIVE_PROJECTS.map((project) => {
-      if (project.symbol === ETokenSymbols.ARAORIG) {
-        return { ...project, buttonName: t('common.swap') };
-      }
-
-      return project;
-    });
-  }, [t]);
+  const activeProjects = useMemo(() => getActiveProjects(t), [t]);
 
   return (
     <div className="grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
--- src/features/show-projects/ui/WaitingProjects.tsx
+++ src/features/show-projects/ui/WaitingProjects.tsx
@@ -1,5 +1,5 @@
-import { FC } from 'react';
-import { Project } from './Project';
+import { FC, useMemo } from 'react';
+import { IProjectProps, Project } from './Project';
 
 import { useTranslation } from 'react-i18next';
 import {
@@ -11,54 +11,67 @@ import {
   COMMON_DELC_DATA,
   COMMON_MS_DATA,
 } from '../../../shared/constants/blockchain';
+import { TFunction } from 'i18next';
+
+const getAwaitingProjects = (t: TFunction): IProjectProps[] => {
+  const projects: IProjectProps[] = [
+    {
+      buttonName: t('common.show'),
+      title: COMMON_CRB_DATA.title,
+      symbol: COMMON_CRB_DATA.symbol,
+      href: COMMON_CRB_DATA.herf,
+    },
+    {
+      buttonName: t('common.show'),
+      title: COMMON_CONS_DATA.title,
+      symbol: COMMON_CONS_DATA.symbol,
+      href: COMMON_CONS_DATA.herf,
+    },
+    {
+      buttonName: t('common.show'),
+      title: COMMON_CONT_DATA.title,
+      symbol: COMMON_CONT_DATA.symbol,
+      href: COMMON_CONT_DATA.herf,
+    },
+    {
+      buttonName: t('common.show'),
+      title: COMMON_DELC_DATA.title,
+      symbol: COMMON_DELC_DATA.symbol,
+      href: COMMON_DELC_DATA.herf,
+    },
+    {
+      buttonName: t('common.show'),
+      title: COMMON_CHAI_DATA.title,
+      symbol: COMMON_CHAI_DATA.symbol,
+      href: COMMON_CHAI_DATA.herf,
+    },
+    {
+      buttonName: t('common.show'),
+      title: COMMON_MS_DATA.title,
+      symbol: COMMON_MS_DATA.symbol,
+      href: COMMON_MS_DATA.herf,
+    },
+    {
+      buttonName: t('common.show'),
+      title: COMMON_CRC_DATA.title,
+      symbol: COMMON_CRC_DATA.symbol,
+      href: COMMON_CRC_DATA.herf,
+    },
+  ];
+
+  return projects;
+};
 
 export const WaitingProjects: FC = () => {
   const { t } = useTranslation();
 
+  const awaitingProjects = useMemo(() => getAwaitingProjects(t), [t]);
+
   return (
     <div className="grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
-      <Project
-        buttonName={t('common.show')}
-        title={COMMON_CRB_DATA.title}
-        symbol={COMMON_CRB_DATA.symbol}
-        href={COMMON_CRB_DATA.herf}
-      />
-      <Project
-        buttonName={t('common.show')}
-        title={COMMON_CONS_DATA.title}
-        symbol={COMMON_CONS_DATA.symbol}
-        href={COMMON_CONS_DATA.herf}
-      />
-      <Project
-        buttonName={t('common.show')}
-        title={COMMON_CONT_DATA.title}
-        symbol={COMMON_CONT_DATA.symbol}
-        href={COMMON_CONT_DATA.herf}
-      />
-      <Project
-        buttonName={t('common.show')}
-        title={COMMON_DELC_DATA.title}
-        symbol={COMMON_DELC_DATA.symbol}
-        href={COMMON_DELC_DATA.herf}
-      />
-      <Project
-        buttonName={t('common.show')}
-        title={COMMON_CHAI_DATA.title}
-        symbol={COMMON_CHAI_DATA.symbol}
-        href={COMMON_CHAI_DATA.herf}
-      />
-      <Project
-        buttonName={t('common.show')}
-        title={COMMON_MS_DATA.title}
-        symbol={COMMON_MS_DATA.symbol}
-        href={COMMON_MS_DATA.herf}
-      />
-      <Project
-        buttonName={t('common.show')}
-        title={COMMON_CRC_DATA.title}
-        symbol={COMMON_CRC_DATA.symbol}
-        href={COMMON_CRC_DATA.herf}
-      />
+      {awaitingProjects.map((project) => (
+        <Project key={project.symbol} {...project} />
+      ))}
     </div>
   );
 };
