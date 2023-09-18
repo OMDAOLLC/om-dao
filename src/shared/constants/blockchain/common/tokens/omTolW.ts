bdec078d41e7904eb69232f1a1496771d7c8be39
mitagmio <mitag.mio@gmail.com>
2023-09-18 15:33:26 +0000

    chore

--- src/shared/constants/blockchain/common/tokens/omTolW.ts
+++ src/shared/constants/blockchain/common/tokens/omTolW.ts
@@ -1,11 +1,11 @@
-import { ITokenConstants } from '../types';
-import { USDT_ABI, WRAPPED_TOKEN_ABI } from '../abis';
+import { ITokenConstants } from "../types";
+import { WRAPPED_TOKEN_ABI } from "../abis";
 
-export const COMMON_TOLW_DATA: Omit<ITokenConstants, 'address'> = {
-  symbol: 'omTolW',
-  abi: WRAPPED_TOKEN_ABI,
-  decimal: '6',
-  herf: 'https://t.me/c/1802432571/661',
-  name: 'OM DAO Wrapped Tollan Worlds',
-  title: 'Tollan Worlds',
+export const COMMON_TOLW_DATA: Omit<ITokenConstants, "address"> = {
+    symbol: "omTolW",
+    abi: WRAPPED_TOKEN_ABI,
+    decimal: "6",
+    herf: "https://t.me/c/1802432571/661",
+    name: "OM DAO Wrapped Tollan Worlds",
+    title: "Tollan Worlds",
 };
--- src/shared/constants/blockchain/common/tokens/omdwCRB.ts
+++ src/shared/constants/blockchain/common/tokens/omdwCRB.ts
@@ -1,11 +1,11 @@
-import { ITokenConstants } from '../types';
-import { USDT_ABI, WRAPPED_TOKEN_ABI } from '../abis';
+import { ITokenConstants } from "../types";
+import { WRAPPED_TOKEN_ABI } from "../abis";
 
-export const COMMON_CRB_DATA: Omit<ITokenConstants, 'address'> = {
-  symbol: 'omdwCRB',
-  abi: WRAPPED_TOKEN_ABI,
-  decimal: '6',
-  herf: 'https://selectedpublic.notion.site/Cross-River-46adffcd372143f3b81e486d518ab222',
-  name: 'OM DAO Wrapped Cross River Bank',
-  title: 'Cross River',
+export const COMMON_CRB_DATA: Omit<ITokenConstants, "address"> = {
+    symbol: "omdwCRB",
+    abi: WRAPPED_TOKEN_ABI,
+    decimal: "6",
+    herf: "https://selectedpublic.notion.site/Cross-River-46adffcd372143f3b81e486d518ab222",
+    name: "OM DAO Wrapped Cross River Bank",
+    title: "Cross River",
 };
