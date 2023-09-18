b0a0e424189a6c7b531513d223ace9c6678f5529
mitagmio <mitag.mio@gmail.com>
2023-09-18 15:43:19 +0000

    show zely

--- src/features/show-projects/ui/ActiveProjects.tsx
+++ src/features/show-projects/ui/ActiveProjects.tsx
@@ -1,33 +1,42 @@
-import { FC, useMemo } from 'react';
-import { IProjectProps, Project } from './Project';
+import { FC, useMemo } from "react";
+import { IProjectProps, Project } from "./Project";
 
-import { useTranslation } from 'react-i18next';
-import { COMMON_AG_DATA } from '../../../shared/constants/blockchain';
-import { TFunction } from 'i18next';
+import { useTranslation } from "react-i18next";
+import {
+    COMMON_AG_DATA,
+    COMMON_ZELY_DATA,
+} from "../../../shared/constants/blockchain";
+import { TFunction } from "i18next";
 
 const getActiveProjects = (t: TFunction) => {
-  const projects: IProjectProps[] = [
-    {
-      title: COMMON_AG_DATA.title,
-      symbol: COMMON_AG_DATA.symbol,
-      href: COMMON_AG_DATA.herf,
-      buttonName: t('common.swap') + ' omAra /',
-    },
-  ];
+    const projects: IProjectProps[] = [
+        {
+            title: COMMON_AG_DATA.title,
+            symbol: COMMON_AG_DATA.symbol,
+            href: COMMON_AG_DATA.herf,
+            buttonName: t("common.swap") + " omAra /",
+        },
+        {
+            title: COMMON_ZELY_DATA.title,
+            symbol: COMMON_ZELY_DATA.symbol,
+            href: COMMON_ZELY_DATA.herf,
+            buttonName: t("common.swap"),
+        },
+    ];
 
-  return projects;
+    return projects;
 };
 
 export const ActiveProjects: FC = () => {
-  const { t } = useTranslation();
+    const { t } = useTranslation();
 
-  const activeProjects = useMemo(() => getActiveProjects(t), [t]);
+    const activeProjects = useMemo(() => getActiveProjects(t), [t]);
 
-  return (
-    <div className="grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
-      {activeProjects.map((project) => (
-        <Project key={project.symbol} {...project} />
-      ))}
-    </div>
-  );
+    return (
+        <div className="grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
+            {activeProjects.map((project) => (
+                <Project key={project.symbol} {...project} />
+            ))}
+        </div>
+    );
 };
